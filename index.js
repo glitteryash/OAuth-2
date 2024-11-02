0d74fe849f2ae945c31b7eb29c7bf4aa2df822b6
glitteryash <glitteryash@gmail.com>
2024-11-02 22:01:25 +0900

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth-route");
+const profileRoute = require("./routes/profile-route");
 const port = 8080;
 dotenv.config();
 require("./config/passport");
@@ -23,6 +24,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRoute);
+app.use("/profile", profileRoute);
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
